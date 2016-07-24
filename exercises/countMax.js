900cae3cc233319bd30c4413a1951495ce323c63
TravisIngram <ingramtravis@gmail.com>
2016-07-24 15:33:07 -0500

    Add JavaScript exercise for countMax

---
+++ exercises/countMax.js
@@ -0,0 +1,31 @@
+/*
+  Function name:  countMax(list);
+  Inputs:         A list of numbers
+  Returns:        The number of times the largest number in the list appears.
+  Prints:         Nothing
+
+  For example,
+    countMax([10, 1, 2, 10, 10]);
+    => 3
+
+  The number "3" is returned since "10" is the largest number in the list and
+  it occurs 3 times.
+
+  ** Note **
+  While it's possible to make use of a function that is contained in another
+  file, that's a topic we'll cover later.
+*/
+
+function countMax(list) {
+
+  // You can write this using nothing more than the 'max' and 'countInList'
+  // functions that you've already written.
+
+  // Remember: inelegant, working code is better than elegant,
+  // unfinished code.
+}
+
+/*
+  I'd advise putting some sanity checks here.
+  How else will you confirm that your code does what you think it does?
+*/
