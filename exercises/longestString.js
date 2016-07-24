860d3c6d07c8443ec071f5d86335851fab099d3f
TravisIngram <ingramtravis@gmail.com>
2016-07-24 15:00:54 -0500

    Update formatting, language of exercises

--- exercises/longestString.js
+++ exercises/longestString.js
@@ -1,26 +1,26 @@
 /*
-# Method name: longest_string(list)
-# Inputs:      a list of strings
-# Returns:     the longest string in the list
-# Prints:      Nothing
-#
-# For example, longest_string(["a", "zzzz", "c"]) should return "zzzz" since
-# the other strings are 1 character long and "zzzz" is 4 characters long.
-#
-# To get the length of a string in the variable str, call str.length
-# For example,
-#   str = "zzzz"
-#   str.length == 4
+  Function name:  longestString(list);
+  Inputs:         A list of strings
+  Returns:        The longest string in the list
+  Prints:         Nothing
+
+  For example, longestString(["a", "zzzz", "c"]) should return "zzzz" since
+  the other strings are 1 character long and "zzzz" is 4 characters long.
+
+  To find the length of the string in the variable myString, call myString.length.
+
+  For example,
+    var myString = "zzzz";
+    console.log(myString.length);
+    => 4
 */
 
 function longestString(list) {
-  // This is your job. :)
+  // This is your job. =]
 
 }
 
-// if __FILE__ == $PROGRAM_NAME
-
 /*
-  # I'd advise putting some sanity checks here.
-  # How else will you be sure your code does what you think it does?
+  I'd advise putting some sanity checks here.
+  How else will you confirm that your code does what you think it does?
 */
--- exercises/min.js
+++ exercises/min.js
@@ -1,18 +1,18 @@
 /*
-# Method name:  min
-# Input:        A list of numbers
-# Returns:      The SMALLEST number in the list
-# Prints:       Nothing
-#
-# In English, the "min" method takes as input a list of numbers and
-# returns the SMALLEST number in that list.
+  Function name:  min(list);
+  Input:          A list of numbers.
+  Returns:        The SMALLEST number in the list.
+  Prints:         Nothing
 
-# Your job here is to fill in the ______.  Be deliberate, don't just guess!
-# The number of spaces (____) is meaningless; it's not as if four blank spots
-# means you need to replace it with a 4-character chunk of JavaScript.
+  In English, the "min" method takes as input a list of numbers and
+  returns the SMALLEST number in that list.
 
-# This is going to be very similar to the previous exercise, max.
-# So don't be afraid if these two methods look almost identical
+  Your job here is to fill in the ____.  Be deliberate, don't just guess!
+  The number of spaces (____) is meaningless; it's not as if four blank spots
+  means you need to replace it with a 4-character chunk of JavaScript.
+
+  This is going to be very similar to the previous exercise, max.
+  So don't be afraid if these two methods look almost identical
 */
 
 function min(list) {
@@ -26,8 +26,8 @@ function min(list) {
   return ____;
 }
 
-// if __FILE__ == $PROGRAM_NAME
-  // "p" prints something to the screen in a way that's better for debugging
+
+// console.log() prints something to the console as a means of basic debugging.
 
 console.log(min([1, 2, 3]) === 1);
 console.log(min([0, -100, 50, -200]) === -200);
@@ -38,16 +38,15 @@ console.log(min([-1]) === -1);
 console.log(min([11, 11, 11]) === 11);
 console.log(min([-22, -11, -22]) === -22);
 
-
 /*
-# Each of the lines above will print out "true" or "false" and collectively
-# act as a sanity check.  Remember that conceptually "x === y"
-# means "are x and y equal?"
-#
-# That is, when you run the code, if any lines print out "false"
-# then you know something is off in your code.
-#
-# Think of these like rumble strips on the side of the road.  They're here
-# to tell you when you're veering off the road, not to guarantee you're
-# driving phenomenally. :)
+  Each of the lines above will print out "true" or "false" and collectively
+  act as a sanity check.  Remember that conceptually "x === y"
+  means "are x and y equal?"
+
+  That is, when you run the code, if any lines print out "false"
+  then you know something is off in your code.
+
+  Think of these like rumble strips on the side of the road.  They're here
+  to tell you when you're veering off the road, not to guarantee you're
+  driving phenomenally. :)
 */
