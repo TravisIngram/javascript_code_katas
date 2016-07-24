62ca719217de6b2c05132b0c7371359ce81a2c12
TravisIngram <ingramtravis@gmail.com>
2016-07-24 15:03:20 -0500

    Add JavaScript exercise for countInList

---
+++ exercises/countInList.js
@@ -0,0 +1,30 @@
+/*
+  Function name:  countInList(list, itemToCount);
+  Inputs:         1. a list of anything, 2. an item for us to count in the list
+  Returns:        The number of times our item is contained in the input list.
+  Prints:         Nothing
+
+  For example,
+    countInList([1,2,3], 1);    == 1
+    countInList([1,2,3], -1);   == 0
+    countInList([1,1,1], 1);    == 3
+
+  --- NOTE ---
+  Some languages, like Ruby and Python, have built in methods to do this.
+  JavaScript does not, so we have to write it ourselves.
+
+  See: http://www.ruby-doc.org/core-2.1.2/Array.html#method-i-count
+*/
+
+function countInList(list, itemToCount) {
+  // You'll need three things:
+  //   1. A running total of the number of times you've seen the item.
+  //   2. A way to loop/iterate through the list.
+  //   3. A way to add to the running total as you see the item.
+
+}
+
+/*
+  I'd advise putting some sanity checks here.
+  How else will you confirm that your code does what you think it does?
+*/
