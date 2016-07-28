f43b5293deeb3668436d5d611b5858d7d92fc34e
TravisIngram <ingramtravis@gmail.com>
2016-07-28 13:42:49 -0500

    Add JavaScript exercise for toEnglish

---
+++ exercises/toEnglish.js
@@ -0,0 +1,48 @@
+/*
+  Function name:  toEnglish(num);
+  Inputs:         A number.
+  Returns:        A String representing the number in English.
+  Prints:         Nothing
+
+  Write a method that takes a positive integer as input and returns the input
+  in "plain English."
+
+  For example,
+
+    toEnglish(1);     == "one"
+    toEnglish(12);    == "twelve"
+    toEnglish(123);   == "one hundred twenty three"
+    toEnglish(1234);  == "one thousand two hundred thirty four"
+    toEnglish(12345); == "twelve thousand three hundred forty five"
+
+  You can decide the largest power of 10 you wish to support, but you should
+  support at least up to the billions.
+
+  ** Hint #1 **
+  Write down a bunch of examples and look for patterns.
+  What words make up "special cases?" There are likely fewer than you think
+  if you can find ways to combine simpler patterns into larger ones.
+
+*/
+
+function toEnglish(num) {
+  // You have to fill in the details here!  =]
+}
+
+/*
+  Write some of your own "sanity checks" here.  Each check should look like:
+
+    console.log(toEnglish(); == ...expected return value...);
+
+  A good sanity check is like a canary in a coal mine: when it fails we know
+  something is very wrong in our code.  This means sanity checks should
+  reflect;
+
+    1. The common cases
+    2. The edge cases
+    3. Likely mistakes you'll make in your code (e.g., using <= instead of <)
+
+  What these cases mean concretely depends on what your function does.
+
+  Hey, there are a bunch of lovely test cases above.  Start with those. =]
+*/
