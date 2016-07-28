d7cdf4c4591bdc3cdd01af3ca19380199ac8fe66
TravisIngram <ingramtravis@gmail.com>
2016-07-28 13:55:21 -0500

    Add JavaScript exercise for toRoman

---
+++ exercises/toRoman.js
@@ -0,0 +1,44 @@
+/*
+  Function name:  toRoman(num)
+  Inputs:         A number.
+  Returns:        A String representing the number in roman numerals.
+  Prints:         Nothing
+
+  toRoman(num); takes a number as input and returns that number using Roman numerals.
+
+  See http://en.wikipedia.org/wiki/Roman_numerals
+
+  It can be tricky to handle the cases where "IV" means 4.  As a first pass,
+  don't hesitate to implement a simpler version where 4 is denoted by "IIII".
+
+  Remember: the priority is FEEDBACK, even if your implementation can't handle
+  every single case.
+*/
+
+function toRoman(num) {
+  // You have to fill in the details here!  =]
+}
+
+/*
+  Write some of your own "sanity checks" here.  Each check should look like:
+
+    console.log(toRoman(); == ...expected return value...);
+
+  A good sanity check is like a canary in a coal mine: when it fails we know
+  something is very wrong in our code.  This means sanity checks should
+  reflect;
+
+    1. The common cases
+    2. The edge cases
+    3. Likely mistakes you'll make in your code (e.g., using <= instead of <)
+
+  What these cases mean concretely depends on what your function does.
+
+  For example,
+
+  roRoman(1);     == "I"
+  roRoman(12);    == "XII"
+  roRoman(123);   == "CXXIII"
+  roRoman(1234);  == "MCCXXXIV"
+  roRoman(12345); == "twelve thousand three hundred forty five"
+*/
