b6525e804340d21c858f14db2b311403cb1a0c08
TravisIngram <ingramtravis@gmail.com>
2016-07-31 14:25:21 -0500

    Add partial JavaScript exercises for URL parsing

---
+++ exercises/findLinks.js
@@ -0,0 +1,35 @@
+/*
+  Function name:  findLinks();
+  Inputs:         A website URL.
+  Returns:        An array of all link URLs on the input website.
+  Prints:         Nothing
+
+
+  ** Note #1 **
+  Since these specific tools aren't available in JavaScript, this particular
+  exercise doesn't directly translate.
+
+  I will see if there is a way to do similar things and up date this accordingly.
+
+  ** Note #2 **
+  You'll need to install the nokogiri package if you haven't, yet.  Run this
+  from the command line to install it:
+
+    npm install nokogiri
+
+  Here are some tutorials on how to use Nokogiri to do this.
+
+    http://ruby.bastardsbook.com/chapters/html-parsing/
+    http://lostechies.com/rodpaddock/2011/04/11/hacking-websites-with-ruby-and-nokogiri/
+*/
+
+function findLinks(url) {
+  // This should return an array of all links at the given URL.
+}
+
+
+/*
+  console.log(findLinks("http://www.cnn.com/").each do |url|
+    puts url
+  end
+*/
