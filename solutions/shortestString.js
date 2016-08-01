71e9cf8414bcf6807f92cea7f3207e3006b407b7
TravisIngram <ingramtravis@gmail.com>
2016-08-01 14:04:26 -0500

    Add completed JS exercise for shortestString

--- solutions/shortestString.js
+++ solutions/shortestString.js
@@ -6,11 +6,30 @@
 */
 
 function shortestString(list) {
-  // This is your job. =]
-
+  var charCount = list[0].length;
+  var shortString = "";
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].length <= charCount) {
+      shortString = list[i];
+      charCount = list[i].length;
+    }
+  }
+  return shortString;
 }
 
 /*
   I'd advise putting some sanity checks here.
   How else will you confirm that your code does what you think it does?
+
+  This is currently only a partial solution.  It doesn't account for multiple
+  strings of the same length.  I'll iterate on it and present a better solution
+  soon.
 */
+
+// console.log() prints something to the console as a means of basic debugging.
+
+console.log(shortestString(["zz", "aaa", "b", "cccc", "FFFFFFFF", "dd"]));
+console.log(shortestString(["zz", "a", "     ", "   ", "  ", "        "]));
+console.log(shortestString(["zzslkfj231lkjdsi", "The quick brown fox", "bee", "Banananananana", "FFFFFFFF", ""]));
+console.log(shortestString(["z", "a", "b", "c", "F", "dd"]));
+console.log(shortestString([0, 213, 12398, 23, 123, 9]));
