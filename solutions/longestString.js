b9233501d3977a19ddc12a9616ba57979bbff335
TravisIngram <ingramtravis@gmail.com>
2016-08-01 14:02:24 -0500

    Add completed JS exercise for longestString

--- solutions/longestString.js
+++ solutions/longestString.js
@@ -15,12 +15,37 @@
     => 4
 */
 
-function longestString(list) {
-  // This is your job. =]
+// function longestString(list) {
+//   var stringCount = 0;
+//   var elementCount = [];
+//   for (var i = 0; i < list.length; i++) {
+//     elementCount.push(list[i].length);
+//   }
+//   console.log(elementCount);
+//   return elementCount;
+// }
 
+function longestString(list) {
+  var charCount = 0;
+  var longString = "";
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].length > charCount) {
+      longString = list[i];
+      charCount = list[i].length;
+    }
+  }
+  return longString;
 }
 
 /*
   I'd advise putting some sanity checks here.
   How else will you confirm that your code does what you think it does?
 */
+
+// console.log() prints something to the console as a means of basic debugging.
+
+console.log(longestString(["zz", "aaa", "b", "cccc", "FFFFFFFF", "dd"]));
+console.log(longestString(["zz", "a", "     ", "   ", "  ", "        "]));
+console.log(longestString(["zzslkfj231lkjdsi", "The quick brown fox", "bee", "Banananananana", "FFFFFFFF", ""]));
+console.log(longestString(["z", "a", "b", "c", "F", "dd"]));
+console.log(longestString([0, 213, 12398, 23, 123, 9]));
