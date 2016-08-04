21244f8c2a7f1bd31884ae30cb7558c4114e3b0c
TravisIngram <ingramtravis@gmail.com>
2016-08-04 14:11:02 -0500

    Add completed JS solution for countInList

--- solutions/countInList.js
+++ solutions/countInList.js
@@ -9,22 +9,34 @@
     countInList([1,2,3], -1);   == 0
     countInList([1,1,1], 1);    == 3
 
-  --- NOTE ---
+  ** Note #1 **
   Some languages, like Ruby and Python, have built in methods to do this.
   JavaScript does not, so we have to write it ourselves.
 
   See: http://www.ruby-doc.org/core-2.1.2/Array.html#method-i-count
+
+  ** Note #2 ***
+  I ran into an issue with this exercise because of where I placed the return
+  statement.  Just something to watch out for.
 */
 
 function countInList(list, itemToCount) {
-  // You'll need three things:
-  //   1. A running total of the number of times you've seen the item.
-  //   2. A way to loop/iterate through the list.
-  //   3. A way to add to the running total as you see the item.
+  var count = 0;
 
+  for (var i = 0; i < list.length; i++) {
+    if (list[i] == itemToCount) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /*
   I'd advise putting some sanity checks here.
   How else will you confirm that your code does what you think it does?
 */
+
+console.log(countInList([1,2,3,1,4,1], 1)   === 3);
+console.log(countInList([1,2,3], -1)  === 0);
+console.log(countInList([1,1,1], 1)   === 3);
+console.log(countInList([], 4)   === 0);
